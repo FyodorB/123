65fb150d74d95c68825179f4436bb837b50322da
FyodorB <97032843+FyodorB@users.noreply.github.com>
2022-07-10 15:33:05 +0700

    add new funcion main_min

--- first/main.py
+++ first/main.py
@@ -2,7 +2,13 @@ def main(a, b):
     return pow(a, 2) + pow(b, 2)
 
 
+text = "new file"
+
+
+def main_min(a, b):
+    return pow(a, 3) - pow(b, 2)
+
+
 if __name__ == "__main__":
     print(main(2, 2))
-
-
+    print(main_min(2, 2))
