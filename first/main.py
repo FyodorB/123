91dd1b5500a3f08d06dd5bc6c64c19c3dfb576f7
FyodorB <97032843+FyodorB@users.noreply.github.com>
2022-07-12 21:05:00 +0700

    add new funcion main_del add new_func

--- first/main.py
+++ first/main.py
@@ -2,13 +2,19 @@ def main(a, b):
     return pow(a, 2) + pow(b, 2)
 
 
-text = "new file"
+def main_multi(a, b):
+    return pow(a, 2) * pow(b, 2)
 
 
 def main_min(a, b):
     return pow(a, 3) - pow(b, 2)
 
+def main_del(a, b):
+    return pow(a, 2) / pow(b, 2)
+
 
 if __name__ == "__main__":
     print(main(2, 2))
     print(main_min(2, 2))
+    print(main_multi(2, 2))
+    print(main_del(2, 1))
---
+++ first/strings.py
@@ -0,0 +1,5 @@
+def new_func(text):
+    return text.upper()
+
+
+print(new_func("hello world"))
