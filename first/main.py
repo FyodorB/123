bd301b0149788e89b06d1fae92e4dd1155c7c9d2
FyodorB <97032843+FyodorB@users.noreply.github.com>
2022-07-12 21:18:40 +0700

    add new funcion main_del add new_func add new func main.py

--- first/main.py
+++ first/main.py
@@ -13,8 +13,13 @@ def main_del(a, b):
     return pow(a, 2) / pow(b, 2)
 
 
+def main_del_first(a, b):
+    return pow(a, 3) / pow(b, 6)
+
+
 if __name__ == "__main__":
     print(main(2, 2))
     print(main_min(2, 2))
     print(main_multi(2, 2))
     print(main_del(2, 1))
+    print(main_del_first(3, 4))
