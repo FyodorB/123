a1534e74b8db70627d82c74361877ef9d68b469a
FyodorB <97032843+FyodorB@users.noreply.github.com>
2022-07-17 22:21:22 +0700

    add task

---
+++ first/exercize_2.py
@@ -0,0 +1,45 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://ru.wikipedia.org/wiki/Категория:Животные_по_алфавиту"
+page = requests.get(url).text
+
+while True:
+    soup = BeautifulSoup(page, 'lxml')
+    animal_category = soup.find("div", class_="mw-category mw-category-columns")
+    category_letter = animal_category.find("div", class_="mw-category-group")
+    choice_letter = category_letter.find_next("h3").text
+    animal_names = animal_category.find_all("li")
+
+    lst_animal =[]
+    dict_animal = {}
+    dict_animal_res = {}
+    value_animal = 0
+    for name in animal_names:
+        lst_animal.append(name.text)
+    for animal in lst_animal:
+        if animal.startswith(choice_letter):
+            value_animal += 1
+    dict_animal.update(({choice_letter: value_animal}))
+
+    for key in dict_animal:
+        try:
+            dict_animal_res[key] += int(dict_animal[key])
+        except:
+            dict_animal_res[key] = int(dict_animal[key])
+
+    for key, value in dict_animal_res.items():
+        print(f"{key}: {value}")
+
+    links = soup.find('div', id='mw-pages').find_all('a')
+    for a in links:
+        if a.text == 'Следующая страница':
+            url = 'https://ru.wikipedia.org/' + a.get('href')
+            page = requests.get(url).text
+
+
+
+
+
+
+
