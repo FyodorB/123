02a85fdcd39229d8717b9419236a6b27caab434b
FyodorB <97032843+FyodorB@users.noreply.github.com>
2022-07-18 20:46:11 +0700

    add more information

--- first/exercise_1.py
+++ first/exercise_1.py
@@ -6,10 +6,9 @@ right = len(a)
 
 while right - left > 1:
     middle = (right + left) // 2
-    if a[middle] == 1:
+    if a[middle] == 0:
         right = middle
     else:
         left = middle
 
-print(a[left])
-print(a[right])
+print(right)
